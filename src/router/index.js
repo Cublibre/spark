12c3c25cd1d7af615f2f9d92121c0015d7b63590
Christine Chen <chenchr@umich.edu>
2020-11-18 19:20:51 -0500

    Set up routing

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+function route(path, component) {
+  /* Creates a route object for the router. */
+  return {
+    path,
+    name: component,
+    component: () => import(`@/pages/${component}`)
+  }
+}
+
+const routes = [
+  route('/', 'Home'),
+  route('/about', 'About'),
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
