5a571c674fc8ec59e56c686de0c83dcb4bb44f95
Christine Chen <chenchr@umich.edu>
2020-11-19 18:03:58 -0500

    Add a search page
    On this page the user inputs a course number they would like to find a partner in.
    When they select a course from the dropdown, the page should switch to
    the Feed with a filtered list of people.
    
    TODO: Feed page, Home page where the user can choose to edit their profile
    or find a partner

---
+++ src/components/CourseSearchbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="searchbar">
+    <b-field label="Enter an EECS course number:">
+      <b-autocomplete
+        v-model="input"
+        :data="filteredCourseCatalog"
+        placeholder="e.g. 493"
+        icon="search"
+        clearable
+        @select="(option) => (showFeed(option))"
+      >
+        <template slot="empty">No EECS classes found with that number.</template>
+      </b-autocomplete>
+    </b-field>
+  </section>
+</template>
+
+<script>
+import router from '@/router'
+export default {
+  name: "CourseSearchbar",
+  data: function () {
+    return {
+      input: "",
+      selected: null,
+    };
+  },
+  methods: {
+    showFeed(code) {
+      // Show feed with filtered users- TODO: pass a course code as prop?
+      console.log(`Selected course: ${code}`)
+      router.push('results')
+    }
+  },
+  computed: {
+    courseCodes () {
+      // TODO: get a list of courses from the database
+      const COURSE_LIST = {
+        "EECS 485": true,
+        "EECS 481": true,
+        "EECS 493": true,
+        "EECS 280": true,
+        "EECS 370": true,
+      }
+      let courseNames = Object.keys(COURSE_LIST)
+      // Extract the numbers from the course name
+      return courseNames.map((name)=>name.slice(-3))
+    },
+    filteredCourseCatalog () {
+      /* Show the matching course codes in the dropdown based on input. */
+      return this.courseCodes.filter((option) => {
+        return (
+          option.toString().toLowerCase().indexOf(this.input.toString().toLowerCase()) >= 0
+        );
+      });
+    }
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,14 +1,26 @@
 <template>
-  <section class="hero is-light" style="width:100vw">
-  <div class="hero-body">
-    <div class="container" >
-      <h1 class="title">
-        What class do you need a partner for?
-      </h1>
-      <h2 class="subtitle">
-        Primary subtitle
-      </h2>
+  <div class="search has-background-white px-6 py-6">
+    <div class="container">
+      <h1 class="title">What class do you need a partner for?</h1>
+      <CourseSearchbar />
     </div>
   </div>
-</section>
 </template>
+
+<script>
+import CourseSearchbar from "@/components/CourseSearchbar";
+export default {
+  components: {
+    CourseSearchbar,
+  },
+};
+</script>
+
+<style scoped>
+.search {
+  border-radius: 15px;
+  -webkit-box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.25);
+  -moz-box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.25);
+}
+</style>
