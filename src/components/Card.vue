f7c5c25d043618dff8587a9524a8d5ff73246d20
Christine Chen <chenchr@umich.edu>
2020-11-19 22:19:48 -0500

    Feed component with working buttons

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="card">
-    <div class="container columns is-mobile is-variable is-align-items-center is-justify-content-center">
+    <div
+      class="container columns is-mobile is-variable is-align-items-center is-justify-content-center"
+    >
       <div class="column is-one-third">
-        <Avatar :url="imageUrl" />
+        <Avatar :url="userData.imageUrl" />
       </div>
       <div class="column is-two-thirds is-flex-direction-column">
         <div class="ml-3">
-          <h1 class="title is-1 has-text-weight-bold">{{ name }}</h1>
-          <h4 class="subtitle is-4 mb-3">{{ major }}, '{{ year.substring(2) }}</h4>
+          <h1 class="title is-1 has-text-weight-bold">{{ userData.name }}</h1>
+          <h4 class="subtitle is-4 mb-3">
+            {{ userData.major }}, '{{ userData.year.substring(2) }}
+          </h4>
           <CourseList :courses="classList" />
           <h6 class="subtitle ml-1 is-7 mb-2 has-text-success">
             <b-icon icon="calendar-check" size="is-small"></b-icon>
             Hover to see available times
           </h6>
-          <Schedule :availableDays="workdays" />
+          <Schedule :availableDays="userData.workdays" />
         </div>
       </div>
     </div>
@@ -26,45 +30,16 @@ import CourseList from "./CourseList.vue";
 import Schedule from "./Schedule.vue";
 export default {
   name: "Card",
+  props: ["userData"],
   components: {
     Avatar,
     CourseList,
     Schedule,
   },
-  data: function () {
-    // Placeholder data for a firebase API call
-    const USER_DATA = {
-      name: "John Doe",
-      major: "Computer Science",
-      year: "2020",
-      imageUrl:
-        "https://post.greatist.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg",
-      courses: {
-        "EECS 485": true,
-        "EECS 493": true,
-        "EECS 376": true,
-      },
-      workdays: {
-        monday: {
-          startTime: "14:00",
-          endTime: "18:00",
-        },
-        wednesday: {
-          startTime: "10:00",
-          endTime: "12:00",
-        },
-        thursday: {
-          startTime: "13:00",
-          endTime: "15:00",
-        },
-      },
-    };
-    return USER_DATA;
-  },
   computed: {
     classList: function () {
       // Get a list of the class names (since their values are all true)
-      return Object.keys(this.courses);
+      return Object.keys(this.userData.courses);
     },
   },
 };
--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,45 +1,53 @@
 <template>
   <div class="is-size-5" style="width: 100%">
-    <DayIcon v-for="(times, index) in week" :key="index" :weekday="weekdays[index]" :timeRange="times" />
+    <DayIcon
+      v-for="(times, index) in weekdayTimes"
+      :key="index"
+      :weekday="weekdays[index]"
+      :timeRange="times"
+    />
   </div>
 </template>
 
 <script>
 import DayIcon from "./DayIcon";
+const index = {
+  monday: 0,
+  tuesday: 1,
+  wednesday: 2,
+  thursday: 3,
+  friday: 4,
+  saturday: 5,
+  sunday: 6,
+};
 export default {
   name: "Schedule",
   components: {
     DayIcon,
   },
   props: ["availableDays"],
-  data: function () {
-    /**
-     *  Start with an empty week, and assign available days from props
-     *  We need a map with all days so the keys stay in order even if
-     *  the props change.
-     *
-     * week is an array because Vue2 doesn't support iterating over
-     * maps or sets in v-for. So strings have to converted to indices
-     */
-    const index = {
-      monday: 0,
-      tuesday: 1,
-      wednesday: 2,
-      thursday: 3,
-      friday: 4,
-      saturday: 5,
-      sunday: 6,
-    };
-    var week = new Array(7);
-    for (const property in this.availableDays) {
-      // week[weekday] = [startTime, endTime]
-      console.log(property, index[property]);
-      week[index[property]] = Object.values(this.availableDays[property]);
-    }
-    return {
-      week,
-      weekdays: Object.keys(index)
-    };
+  computed: {
+    weekdayTimes: function () {
+      /**
+       *  Start with an empty week, and assign weekdays time ranges.
+       *  We need a map with all days so the weekdays stay in order even if
+       *  the available days change.
+       *
+       *  week is an array of arrays, i.e. week[0] = ["0:00","1:00"] means
+       *  Monday from 12AM to 1AM. Vue 2 doesn't support iterating over
+       *  maps or sets in v-for.
+       */
+
+      var week = new Array(7);
+      for (const weekday in this.availableDays) {
+        // week[weekday] = [startTime, endTime]
+        week[index[weekday]] = Object.values(this.availableDays[weekday]);
+      }
+      return week;
+    },
+    weekdays: function () {
+      return Object.keys(index);
+    },
   },
 };
 </script>
